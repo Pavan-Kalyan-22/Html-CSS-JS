3ae688ef02fb41e0fbfb4b2f8ae4f91bf1fb4e0a
Pavan-Kalyan-22 <152496740+Pavan-Kalyan-22@users.noreply.github.com>
2023-12-19 20:48:12 +0530

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,8 @@
+		function imageChange(anything){
+			document.querySelector('.starbucks').src = anything;
+		}
+
+		function changeColor(color){
+			const circle = document.querySelector('.circle');
+			circle.style.background = color;
+		}
